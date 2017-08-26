46964f5258dbf40e65baed71502b99bd3a2009b8
Vukasinn <kravica1231@gmail.com>
2017-08-27 00:29:54 +0200

    The select is working properly when checking something,the other one isn't checked

--- js/app.js
+++ js/app.js
@@ -15,18 +15,46 @@ $(".swap").on('click',function(){
 // 	console.log(toAdd);
 // });
 
+var changeCurrency = function (event) {
+    var cash = ['RSD', 'EUR', 'USD'];
+    $("#options2").empty();   
+    var currency;
+    if(event){
+        currency = $(this).val().toUpperCase();
+    } else{
+        currency = 'EUR';
+    }
+    
+    if(cash.includes(currency)){
+        var index = cash.indexOf(currency);
+        cash.splice(index, 1);
+    }
+    for(var i = 0; i <= 2; i++){
+        var option = $("<option>");
+            option.attr({
+                "value":cash[i],
+                "class":cash[i]
+            });
+            option.html(cash[i]);
+        $("#options2").append(option);    
+    }
+}
+changeCurrency();
+$('#options1').on('change', changeCurrency);
+
 $.ajax({
     url:'https://api.kursna-lista.info/b7b80a59415046c33449b6a2a96bd4d8/kursna_lista',
     method:'GET',
-    dataType:'jsonp'
-    
+    dataTypes:'jsonp'
 })
 .done(function(data){
-    console.log(data);
+    console.log(data.result);
 })
 .catch(function(err){
     console.log(err);
 })
+
+
 
 // let getRates = (from,to) => {
 // 	let rates = document.createElement('rates');
@@ -43,6 +71,9 @@ let switchCurrency = () => {
 	let temp = document.querySelector(".options1").value;
 	document.querySelector('.options1').value = document.querySelector('.options2').value;
 	document.querySelector('.options2').value = temp;
+	let temp2 = document.querySelector('.firstInput').value;
+	document.querySelector('.firstInput').value = document.querySelector('.secondInput').value;
+	document.querySelector('.secondInput').value = temp2;
 }
 
 
